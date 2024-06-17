6c380dceaa26f83a5a77d8916565b12162fe041f
Revanth Madupoju <revanthmadupoju@gmail.com>
2024-06-17 22:43:09 +0530

    Add files via upload

---
+++ Medical Data Visualizer freecodecamp.org Assignment/medical_data_visualizer.py
@@ -0,0 +1,58 @@
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+import numpy as np
+
+# Import data
+df = pd.read_csv('medical_examination.csv')
+
+# Add 'overweight' column
+df['overweight'] = (df['weight'] / (df['height'] / 100) ** 2 > 25).astype(int)
+
+# Normalize data by making 0 always good and 1 always bad. If the value of 'cholesterol' or 'gluc' is 1, make the value 0. If the value is more than 1, make the value 1.
+df['cholesterol'] = df['cholesterol'].apply(lambda x: 0 if x == 1 else 1)
+df['gluc'] = df['gluc'].apply(lambda x: 0 if x == 1 else 1)
+
+
+# Draw Categorical Plot
+def draw_cat_plot():
+
+  df_cat = pd.melt(df, id_vars=['cardio'],
+                   value_vars=['cholesterol', 'gluc', 'smoke', 'alco', 'active', 'overweight'])
+
+  df_cat = df_cat.groupby(['cardio', 'variable', 'value']).size().reset_index()
+  df_cat = df_cat.rename(columns={0: 'total'})
+
+  catplot = sns.catplot(data=df_cat, kind="bar", x="variable", y="total", hue="value", col="cardio")
+  fig = catplot.fig
+
+  # Do not modify the next two lines
+  fig.savefig('catplot.png')
+  return fig
+
+
+# Draw Heat Map
+def draw_heat_map():
+    # Clean the data
+    df_heat = df[(df['ap_lo'] <= df['ap_hi']) &
+     (df['height'] >= df['height'].quantile(0.025)) &
+     (df['height'] <= df['height'].quantile(0.975)) &
+     (df['weight'] >= df['weight'].quantile(0.025)) &
+     (df['weight'] <= df['weight'].quantile(0.975))
+     ]
+
+    # Calculate the correlation matrix
+    corr = df_heat.corr()
+
+    # Generate a mask for the upper triangle
+    mask = np.triu(np.ones_like(corr, dtype=bool))
+
+    # Set up the matplotlib figure
+    fig, ax = plt.subplots(figsize=(16, 9))
+
+    # Draw the heatmap with 'sns.heatmap()'
+    sns.heatmap(corr, mask=mask, square=True, linewidths=0.5, annot=True, fmt="0.1f")
+
+    # Do not modify the next two lines
+    fig.savefig('heatmap.png')
+    return fig
